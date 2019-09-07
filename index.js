5da1579d159d735f7ee019460d6bbffea1d2927f
amit <amit.kuntal@mountblue.io>
2019-09-07 16:26:59 +0530

    error handling added in file reading and writing function

--- index.js
+++ index.js
@@ -4,26 +4,36 @@ const csv=require('csvtojson')
 const fs = require("fs");
 
 const filepath='./data-set/matches.csv';
+try{
+    csv().fromFile(filepath).then((matchesJson)=>{
+        dataprocessingFunctions.matchesPerYear(matchesJson);
+        dataprocessingFunctions.matchesWonPerTeamForAllYear(matchesJson);
+        csv().fromFile('./data-set/deliveries.csv').then((deliveriesJson)=>{
+          dataprocessingFunctions.extraRunConductedInYear(matchesJson,deliveriesJson,'2016');
+          dataprocessingFunctions.economicalBowlersInYears(matchesJson,deliveriesJson,'2015')
 
-csv().fromFile(filepath).then((matchesJson)=>{
-    dataprocessingFunctions.matchesPerYear(matchesJson);
-    dataprocessingFunctions.matchesWonPerTeamForAllYear(matchesJson);
-    csv().fromFile('./data-set/deliveries.csv').then((deliveriesJson)=>{
-      dataprocessingFunctions.extraRunConductedInYear(matchesJson,deliveriesJson,'2016');
-      dataprocessingFunctions.economicalBowlersInYears(matchesJson,deliveriesJson,'2015')
-
+        })
+        
     })
-    
-})
+}
+catch(err){
+  console.log(err);
+}
 
-function writeJSONFile(result,JSONfilename){
-    fs.writeFile("./output/"+JSONfilename+".json", JSON.stringify(result), (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        };
-        console.log(JSONfilename+".json  created");
-    });
+try{
+    function writeJSONFile(result,JSONfilename){
+        fs.writeFile("./output/"+JSONfilename+".json", JSON.stringify(result), (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            };
+            console.log(JSONfilename+".json  created");
+        });
+    }
+}
+catch(err)
+{
+  console.log(err);
 }
 module.exports.writeJSONFile=writeJSONFile;
 
